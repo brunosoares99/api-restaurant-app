417ae1250958df97aa4cefc6cf6269ac8f3648b5
brunosoares99 <bruno.soares@tindin.com.br>
2022-04-26 19:34:51 -0300

    fix: fix route /order/done to only save when has some item

--- src/services/orders/SaveOrderService.ts
+++ src/services/orders/SaveOrderService.ts
@@ -6,6 +6,12 @@ interface OrderRequest {
 
 class SaveOrderService {
   async  execute({ order_id }: OrderRequest){
+    const items = await prismaClient.item.count({
+      where:{
+        order_id: order_id
+      }
+    })
+    if(items < 1) throw new Error("A ordem precisa de no minimo um item para ser salva.");
     const order = await prismaClient.order.update({
       where:{
         id: order_id
